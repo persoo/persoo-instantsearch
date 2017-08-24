0f475106da18b78485621663b195945a6828f055
Jakub Černý <kuba@persoo.cz>
2017-08-24 15:45:38 +0200

    BUGFIX - remove /sw.js from production version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -73,12 +73,7 @@ module.exports = {
         new CopyWebpackPlugin([
             { from: './manifest.json', to: './' },
             { from: './favicon.ico', to: './' }
-        ]),
-        new OfflinePlugin({
-            relativePaths: false,
-            AppCache: false,
-            publicPath: '/'
-        })
+        ])
     ]).concat(ENV==='production' ? [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
@@ -89,6 +84,11 @@ module.exports = {
                 "\n© Persoo, s.r.o; Licensed MIT | github.com/persoo/persoo-instantsearch." +
                 "\nWrapper, extensions, improvements to Algolia's instantsearch.js.", {})
     ] : [
+        new OfflinePlugin({
+            relativePaths: false,
+            AppCache: false,
+            publicPath: '/'
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: true },
             global_defs: { DEBUG: true }
